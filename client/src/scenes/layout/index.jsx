0f92a075ba4e14e04b28fb0fd015f6a450123933
Bryan <34315902+bryan-padero@users.noreply.github.com>
2022-12-12 21:28:19 +0400

    added Navbar and Sidebar components

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, useMediaQuery } from "@mui/material"
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
-function Layout() {
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 1000px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div>Layout</div>
-  )
+    <Box display={ isNonMobile ? "flex" : "block" }width ="100%" height="100%">
+      <Sidebar 
+        isNonMobile={ isNonMobile }
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+         />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
 
 export default Layout;
