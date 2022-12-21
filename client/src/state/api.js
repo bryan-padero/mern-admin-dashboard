6644b47569eec238913c4e70d6a00d1e5ab64dde
Bryan <34315902+bryan-padero@users.noreply.github.com>
2022-12-21 13:09:26 +0400

    added performance and dashboard routes

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales", "Admins"],
+    tagTypes: [
+    "User", 
+    "Products", 
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales", 
+    "Admins", 
+    "Performance",
+    "Dashboard"    
+    ],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -33,9 +43,17 @@ export const api = createApi({
             query: () => "sales/sales",
             providesTags: ["Sales"]
         }),
-        getAdmins: build.query ({
+        getAdmins: build.query({
             query: () => "management/admins",
             providesTags: ["Admins"]
+        }),
+        getUserPerformance: build.query({
+            query: (id) => `management/performance/${id}`,
+            providesTags: ["Performance"]
+        }),
+        getDashboard: build.query({
+            query: () => "general/dashboard",
+            providesTags: ["Dashboard"]
         })
     })
 })  
@@ -48,4 +66,6 @@ export const {
     useGetGeographyQuery,
     useGetSalesQuery,
     useGetAdminsQuery,
+    useGetUserPerformanceQuery,
+    useGetDashboardQuery
  } = api
--- server/index.js
+++ server/index.js
@@ -15,8 +15,9 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import OverallStat from "./models/OverallStat.js"
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js";
 
 
 // CONFIGURATIONS
@@ -51,6 +52,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // User.insertMany(dataUser);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
 }).catch((err) => {
     console.log(`${err} did not conenct`);
 });
